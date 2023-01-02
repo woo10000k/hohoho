8d324f05e708c7821a21d7558a2d626a037a76c3
woo10000k <109941023+woo10000k@users.noreply.github.com>
2023-01-03 01:30:06 +0900

    Update gcdlcm.py

--- gcdlcm.py
+++ gcdlcm.py
@@ -1,15 +1,27 @@
+import sys
+input=sys.stdin.readline
+
 def gcd(A,B):
   if max(A,B)%min(A,B)==0:
     return min(A,B)
   elif max(A,B)%min(A,B)!=0:
-    return gcd(max(A,B), max(A,B)%min(A,B))
+    return gcd(max(A,B), int(max(A,B)%min(A,B)))
   else:
     return min(A,B)
     
 
 i=0
-T=int(input())
-for i in range(T):
-  A,B = map(int, input().split())
-  print(gcd(A,B))
-  i+1
+
+N=int(input())
+arr=[[*map(int,input().split())] for _ in range(N*2)]
+
+
+#print(arr[1][0],arr[1][1])
+
+for i in range(N*2):
+  if not arr[i]:
+    print("")
+  else:  
+    A, B = (int(arr[i][0]), int(arr[i][1]))
+    print(gcd(A,B),int((A*B)/gcd(A,B)))
+    i+1
