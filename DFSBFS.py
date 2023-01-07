1711614bfc823c9220db2df0a90808784f6eb01e
woo10000k <109941023+woo10000k@users.noreply.github.com>
2023-01-07 15:28:00 +0900

    Create DFSBFS.py
    프로그래머스 깊이/너비 우선 탐색

---
+++ DFSBFS.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+def solution(numbers, target):
+    answer = 0
+    queue = deque([(0, 0)]) # sum, level
+    while queue:
+        s, l = queue.popleft()
+        if l > len(numbers):
+            break
+        elif l == len(numbers) and s == target:
+            answer += 1
+        queue.append((s+numbers[l-1], l+1))
+        queue.append((s-numbers[l-1], l+1))
+
+    return answer
