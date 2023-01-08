5579ade5be9e251e78677b8236a686e92fe57413
woo10000k <109941023+woo10000k@users.noreply.github.com>
2023-01-08 20:41:55 +0900

    Create solution.py
    프로그래머스 체육복

---
+++ solution.py
@@ -0,0 +1,32 @@
+def solution(n, lost, reserve):
+    lost.sort()
+    reserve.sort()
+    answer = n-len(lost)
+    imsi=[]
+    i=0
+    while i<len(lost):
+        if reserve.count(lost[i]) == 1:
+            reserve.remove(lost[i])
+            imsi.append(lost[i])
+            answer += 1
+            i += 1
+        else:
+            i += 1  
+    
+    lost = [x for x in lost if x not in imsi]
+    
+    i=0
+    while i<len(lost):
+        if reserve.count(lost[i]-1) == 1:
+            reserve.remove(lost[i]-1)
+            answer += 1
+            i += 1
+        elif reserve.count(lost[i]+1) == 1:
+            reserve.remove(lost[i]+1)
+            answer += 1
+            i += 1
+    
+        else:
+            i += 1
+    
+    return answer
