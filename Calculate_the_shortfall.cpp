466a75f8e695a792b8dfdf67746d771cac7a4189
woo10000k <109941023+woo10000k@users.noreply.github.com>
2023-01-16 16:21:33 +0900

    Create Calculate_the_shortfall.cpp

---
+++ Calculate_the_shortfall.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+
+long long solution(int price, int money, int count){
+    long long answer = 0;
+    for (int i=1; i < count+1; i++)
+    {
+        answer = answer + (i*price);
+    }
+    if(answer <= money)
+    {
+        answer = 0;
+    }
+    if(answer > money)
+    {
+        answer = answer - money;
+    }    
+    return answer;
+}
+
+//프로그래머스 Lv.1 부족한 금액 계산하기
