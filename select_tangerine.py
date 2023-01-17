6b3dd0e714241060f994c288a49d06069cc7249a
woo10000k <109941023+woo10000k@users.noreply.github.com>
2023-01-17 23:55:55 +0900

    Create select_tangerine.py

---
+++ select_tangerine.py
@@ -0,0 +1,10 @@
+def solution(k, tangerine):
+    answer = 0
+    tangerine_setlist=list(set(tangerine)
+    answer = len(tangerine_setlist)
+    
+    return answer
+                           
+                           
+                           
+#프로그래머스 lv2 귤고르기
