2966394f0981b22e648f72680e72c74f6b5c4988
woo10000k <109941023+woo10000k@users.noreply.github.com>
2023-01-18 01:21:29 +0900

    Update select_tangerine.py

--- select_tangerine.py
+++ select_tangerine.py
@@ -1,10 +1,24 @@
 def solution(k, tangerine):
     answer = 0
-    tangerine_setlist=list(set(tangerine)
-    answer = len(tangerine_setlist)
-    
+    tangerine_dict = {}
+    for i in range(len(tangerine)):
+        if tangerine[i] not in tangerine_dict:
+            tangerine_dict[tangerine[i]] = 1
+        elif tangerine[i] in tangerine_dict:
+            tangerine_dict[tangerine[i]] +=1
+
+
+
+    while k>=1:
+        tan_max=tangerine_dict[max(tangerine_dict, key=tangerine_dict.get)]
+#        print(tan_max)
+        k = k-(tan_max)
+        print(k)
+        tangerine_dict.pop(max(tangerine_dict, key=tangerine_dict.get))
+        answer +=1
+        
     return answer
                            
                            
-                           
 #프로그래머스 lv2 귤고르기
+#시간
