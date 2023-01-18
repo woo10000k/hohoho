7f75a430269118f7b2ff836a041b145dea066dce
woo10000k <109941023+woo10000k@users.noreply.github.com>
2023-01-18 14:42:24 +0900

    Update select_tangerine.py

--- select_tangerine.py
+++ select_tangerine.py
@@ -7,8 +7,16 @@ def solution(k, tangerine):
         elif tangerine[i] in tangerine_dict:
             tangerine_dict[tangerine[i]] +=1
 
+    tangerine_dict_list=list(tangerine_dict.values())
+    tangerine_dict_list.sort(reverse=True)
+    i=0
+    while k>=1:
+        k -=tangerine_dict_list[i]
+        answer +=1
+        i +=1
+            
 
-
+'''
     while k>=1:
         tan_max=tangerine_dict[max(tangerine_dict, key=tangerine_dict.get)]
 #        print(tan_max)
@@ -18,7 +26,10 @@ def solution(k, tangerine):
         answer +=1
         
     return answer
-                           
+'''           
                            
 #프로그래머스 lv2 귤고르기
-#시간
+#시간초과
+
+#while 돌기 전에 dict 에서 value -> list -> sort 후 while // 시간 통과
+#아래 내용은 value 최대값을 계속 찾아야 하기 때문에 dict를 while 돌때마다 탐색, 미리 정렬해 놓으면 한번만 탐색후 1 
