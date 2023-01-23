3b5928d2e84c7fa788578e23d045d9fb859e93c4
woo10000k <109941023+woo10000k@users.noreply.github.com>
2023-01-23 21:51:39 +0900

    Update coingame.py

--- coingame.py
+++ coingame.py
@@ -47,5 +47,49 @@ for i in range(N*2):
 3 3 3 3
 
 2 3 1 2
+
+-----------------------------------------------------------------------------------------fix
+
+import sys
+input=sys.stdin.readline
+
+def coingame(A,B,C,D):
+    if A > B:
+        if D==0:
+            if A > B+C:
+                return "N"
+
+        return coingame(A, B+C ,D,0)
+    elif A < B:
+        
+        return coingame(A+C, B, D, 0)
+    
+    elif A == B:
+        if D==0:
+            return A,B+C
+        return coingame(A,B+C,D,0)
+        
+
+print(coingame(2,3,3,3))
+
+#i=0
+
+#N=int(input())
+#arr=[[*map(int,input().split())] for _ in range(N*2)]
 '''
+
+
+
+
+'''
+for i in range(N*2):
+  if not arr[i]:
+    print("")
+  else:  
+    A, B, C, D = (int(arr[i][0]), int(arr[i][1]), int(arr[i][2]), int(arr[i][3]))
+    print(coingame(A,B,C,D))
+    i+1
+'''
+
+
 
