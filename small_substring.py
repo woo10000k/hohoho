8e452b5e9da47167e722cb06c6f2cbadca3920c3
woo10000k <109941023+woo10000k@users.noreply.github.com>
2023-01-26 14:19:40 +0900

    Update small_substring.py

--- small_substring.py
+++ small_substring.py
@@ -1,4 +1,17 @@
 def solution(t, p):
     answer = 0
-    
+    t_list = list(t)
+    p_list = list(p)
+    for i in range(len(t_list)-len(p_list)+1):
+        t_str = ""
+        for i in range(i, i+len(p_list)):
+            t_str += t_list[i]
+
+        t_int = int(t_str)
+        p_int = int(p)
+        if t_int <= p_int:
+            answer += 1
+        
     return answer
+
+#프로그래머스 lv1 크기가 작은 부분 문자
